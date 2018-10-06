7423af8aedd973d4217d21d9f0589a5b0ee0db24
ello <e.olivarriaa@gmail.com>
2018-10-06 12:36:02 -0700

    Angular project files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+//fiel that supports root components that forms part of the angular app
+import { Component } from '@angular/core';//component class from angular core library
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+@Component({//prepare app component class, component decorator, js object as parameter
+  selector: 'app-root',//in index.html file also includes it, where the view of this component is displayed in browser
+  templateUrl: './app.component.html',//template corresponding to this compoennet, template defines the view for this component
+  styleUrls: ['./app.component.scss']//scss for component template
 })
-export class AppComponent {
-  title = 'conFusion';
+export class AppComponent {//export the class
+  title = 'conFusion'; //display this is the brower, display in the view
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+
+import 'hammerjs';
+
+import { MenuComponent } from './menu/menu.component';
+import { MatListModule } from '@angular/material/list';
+
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
